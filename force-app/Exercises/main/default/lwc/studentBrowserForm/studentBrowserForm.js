e1a74c5e7ecf2a071b590f501a8152f083b37754
Sreelaxmi <hansikapg7@gmail.com>
2023-03-21 13:53:20 -0400

    Completed 2-9 Completed

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -1,13 +1,16 @@
 import { LightningElement ,wire} from 'lwc';
 import getInstructors from '@salesforce/apex/StudentBrowserForm.getInstructors';
 import getDeliveriesByInstructor from '@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor';
-
-export default class StudentBrowserForm extends LightningElement {
+import {NavigationMixin} from 'lightning/navigation';
+import {encodeDefaultFieldValues} from 'lightning/pageReferenceUtils';
+export default class StudentBrowserForm extends NavigationMixin(LightningElement) {
     instructors =[];
     selectedInstructorId = '';
     deliveries = [];
     selectedDeliveryId = '';
     Error;
+    isButtonDisabled=true;
+
     @wire(getInstructors)
     wired_getInstructors({error,data}){
         debugger;
@@ -52,6 +55,7 @@ export default class StudentBrowserForm extends LightningElement {
     onInstructorChange(event){
         this.selectedDeliveryId='';
         this.selectedInstructorId=event.target.value;
+        this.isButtonDisabled = (this.selectedInstructorId === '');
         this.notifyParent();
     }
     onDeliveryChange(event){
@@ -66,5 +70,22 @@ export default class StudentBrowserForm extends LightningElement {
             }
         });
         this.dispatchEvent(evt);
+    }
+    onAddNewDelivery () {
+        // Opens the new Course Delivery record modal dialog 
+        // with the selected InstructorId prepopulated 
+        const pageInfo = {
+            type: "standard__objectPage",
+            attributes: {
+                objectApiName: "Course_Delivery__c",
+                actionName: "new"
+            },
+            state: {
+                defaultFieldValues: encodeDefaultFieldValues({
+                    Instructor__c: this.selectedInstructorId
+                })
+            }
+        };
+        this [NavigationMixin.Navigate] (pageInfo);
     }
 }
