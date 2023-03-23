841a0b1c3fe5d345e56cbb383155f014449d16ce
Sreelaxmi <hansikapg7@gmail.com>
2023-03-23 13:56:09 -0400

    Exercise 5-3 completed

---
+++ force-app/Exercises/main/default/lwc/utils/utils.js
@@ -0,0 +1,15 @@
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import { getFieldValue, getFieldDisplayValue} from 'lightning/uiRecordApi';
+export default class Utils  {
+    static showToast = (firingComponent, toastTitle,toastBody,variant) =>{
+        const evt = new ShowToastEvent({
+            title: toastTitle,
+            message:toastBody,
+            variant: variant
+        });
+        firingComponent.dispatchEvent(evt);
+    }
+     static getDisplayValue (data,field){
+        return getFieldDisplayValue(data,field) ? getFieldDisplayValue(data,field): getFieldValue(data,field);
+     }
+}
